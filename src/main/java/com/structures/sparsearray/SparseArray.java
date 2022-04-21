28fafd72d93c26a0947363eebc693abcd0387262
wendongchao <dongchao1860369@sina.com>
2022-04-22 00:43:25 +0800

    添加注释

--- src/main/java/com/structures/sparsearray/SparseArray.java
+++ src/main/java/com/structures/sparsearray/SparseArray.java
@@ -2,6 +2,15 @@ package com.structures.sparsearray;
 
 /**
  * 稀疏数组
+ * 二维数组 转 稀疏数组的思路
+ * 1. 遍历  原始的二维数组，得到有效数据的个数 sum
+ * 2. 根据sum 就可以创建 稀疏数组 sparseArr   int[sum + 1] [3]
+ * 3. 将二维数组的有效数据数据存入到 稀疏数组
+ *
+ * 稀疏数组转原始的二维数组的思路
+ *
+ * 1. 先读取稀疏数组的第一行，根据第一行的数据，创建原始的二维数组，比如上面的  chessArr2 = int [11][11]
+ * 2. 在读取稀疏数组后几行的数据，并赋给 原始的二维数组 即可.
  */
 public class SparseArray {
     public static void main(String[] args) {
